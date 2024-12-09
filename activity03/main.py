007f888f6f6bbbef62cc4cc1b3b78bf205a2f578
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-12-09 16:57:07 -0300

    Update main.py

--- activity03/main.py
+++ activity03/main.py
@@ -36,20 +36,20 @@ def calcular_areas():
         elif opcao == "2": 
             num1= float(input("digite o menor lado do retangulo: "))
             num2= float(input("digite o maior lado do retangulo: "))
-            print(f"O resultado da subtração é: {arearetangulo(num1, num2)}")
+            print(f"A área do retangulo é: {arearetangulo(num1, num2)}")
         elif opcao == '3':
             num1= float(input("digite o raio do circulo: "))
-            print(f"O resultado da multiplicação é: {areacirculo(num1)}")
+            print(f"A área do circulo é: {areacirculo(num1)}")
         elif opcao == '4':
             num1= float(input("digite o raio do circulo: "))
-            print(f"O resultado da divisão é: {pericirculo (num1)}")
+            print(f"A área do circulo é: {pericirculo (num1)}")
         elif opcao == '5':
             num1= float(input("digite o menor lado do retangulo: "))
             num2= float(input("digite o maior lado do retangulo: "))
-            print(f"O resultado da divisão é: {periretangulo(num1, num2)}")
+            print(f"A área do retangulo é: {periretangulo(num1, num2)}")
         elif opcao == '6':
             num1= float(input("digite um lado do quadrado: "))
-            print(f"O resultado da divisão é: {periquadrado(num1)}")
+            print(f"A área do quadrado é: {periquadrado(num1)}")
         else:
             print(f"Opção inválida!")
 
